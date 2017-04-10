6362dc7679e4b38b5a4de9f81389fecb7d9f02d4
Alex <alex.mahinko@gmail.com>
2017-04-10 14:23:20 -0400

    Kittens page now refactored, will return maximum kittens as efficently as possible.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
 def welcome
 
@@ -15,11 +16,19 @@ def contest
 end
 
 def kitten
+# set_kitten_url
+
+end
+
+def kittens
+# set_kitten_url
+
+end
+
+def set_kitten_url
 
   requested_size = params[:size]
-
   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats/"
-
 end
 
 end
